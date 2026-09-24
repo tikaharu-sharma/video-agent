eb7087708127b24298e5589d356606ce7a3a8c51
Tikaharu Sharma <tgaire13@gmail.com>
2026-09-24 01:49:51 -0400

    Add Youtube audio download and extraction

---
+++ utils/audio_processor.py
@@ -0,0 +1,26 @@
+import yt_dlp
+from pydub import AudioSegment
+import os
+
+DOWNLOAD_DIR = 'downloads'
+os.makedirs(DOWNLOAD_DIR, exist_ok=True)
+
+def download_youtube_audio(url :str) ->str:
+    output_path = os.path.join(DOWNLOAD_DIR, '%(title)s.%(ext)s')
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'outtmpl': output_path,
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'wav',
+            'preferredquality': '192',
+        }],
+        "quiet": True,
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url, download=True)
+        filename = ydl.prepare_filename(info).replace('.webm', '.wav').replace('.m4a', '.wav')
+    return filename
+
+download_youtube_audio("https://www.youtube.com/watch?v=cPn5MFYye0s")
